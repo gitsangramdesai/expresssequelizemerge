7573e1dbb9d0a9cc33d1c2e629360c6de594029b
sangram <mail.sangram.desai@gmail.com>
2016-08-08 17:30:58 +0530

    gulp task for create log dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,40 @@
 var gulp = require('gulp');
-var gulpZip = require('gulp-zip');
 var del = require("del");
 var zip = require("gulp-zip");
-var clean = require('gulp-clean');
 var dateFormat = require('dateformat');
-
+var fs = require('fs');
 var zipFileName = dateFormat((new Date()), "yyyymmdd");
 
-console.log("spare todays log file access_" + zipFileName + '.log');
+//create log directories
+gulp.task('createLogDir', function () {
+    if (!fs.existsSync('./logs/access/')) {
+        fs.mkdirSync('./logs/access/');
+    }
+     if (!fs.existsSync('./logs/info/')) {
+        fs.mkdirSync('./logs/info/');
+    }
+     if (!fs.existsSync('./logs/error/')) {
+        fs.mkdirSync('./logs/error/');
+    }
+});
 
 //task ziplogs & logcleanup should be called sequentially
-gulp.task('ziplogs', function () {
-    return  gulp.src([
+gulp.task('ziplogs', ['createLogDir'], function () {
+    return gulp.src([
         'logs/access/**/*.log',
-        '!logs/access/access_'+ zipFileName + '.log'
-        ])
-        .pipe(zip(zipFileName+'.zip'))
+        '!logs/access/access_' + zipFileName + '.log'
+    ])
+        .pipe(zip(zipFileName + '.zip'))
         .pipe(gulp.dest('./backup'));
 });
 
 //it requires ziplogs task to be completed
 gulp.task('logcleanup', ['ziplogs'], function () {
-   return del([
-    'logs/access/**/*.log',
-    '!logs/access/access_'+ zipFileName + '.log'
-  ]);
+    return del([
+        'logs/access/**/*.log',
+        '!logs/access/access_' + zipFileName + '.log'
+    ]);
 });
 
-gulp.task('default', ['ziplogs','logcleanup']);
+gulp.task('default', ['createLogDir','ziplogs', 'logcleanup']);
+//gulp.task('default', ['createLogDir']);
